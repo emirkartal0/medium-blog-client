45945d797cee986bdff6e448929f0813638217e6
emir <emirkartal00@gmail.com>
2023-06-11 23:38:27 +0300

    feat: :sparkles: navigate home after auth

--- src/app/navbar/login/login.component.ts
+++ src/app/navbar/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { SessionStorageService } from 'src/app/services/session-storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,13 +13,17 @@ export class LoginComponent {
 
   constructor(
     private http: HttpClient,
-    private sessionStorageService: SessionStorageService
+    private sessionStorageService: SessionStorageService,
+    private router: Router
   ) {}
 
 
   login(data: NgForm) {
     this.http.post<any>('https://localhost:7118/api/Auth/login', data).subscribe(data => {
       this.sessionStorageService.setItem('token', data.token);
+      if (data.token) {
+        this.router.navigateByUrl('/home')
+      }
     })
   }
 
--- src/app/navbar/register/register.component.ts
+++ src/app/navbar/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { SessionStorageService } from 'src/app/services/session-storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -12,12 +13,16 @@ export class RegisterComponent {
   
   constructor(
     private http: HttpClient,
-    private sessionStorageService: SessionStorageService
+    private sessionStorageService: SessionStorageService,
+    private router: Router
   ) {}
 
   register(data: NgForm) {
       this.http.post<any>('https://localhost:7118/api/Auth/register', data).subscribe(data => {
         this.sessionStorageService.setItem('token', data.token);
+        if (data.token) {
+          this.router.navigateByUrl('/home')
+        }
     })
   }
 
