7735f69133b0de393a9f88dbefc529c7e3f3481c
emir <emirkartal00@gmail.com>
2023-06-11 17:12:35 +0300

    feat: :lipstick: add article component template

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-article',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent {
+
+  //@Input() article: any;
+
+  article: any = {
+    Owner: "Emir Kartal",
+    CreatedTime: "Feb 27",
+    Title: "How to make PHP 2000 times faster",
+    Content: "We’ll discuss why PHP is an awesome programming language, how to make your applications faster in means of performance and development.We’ll discuss why PHP is an awesome programming language, how to make your applications faster in means of performance and development.We’ll discuss why PHP is an awesome programming language, how to make your applications faster in means of performance and development.We’ll discuss why PHP is an awesome programming language, how to make your applications faster in means of performance and development.We’ll discuss why PHP is an awesome programming language, how to make your applications faster in means of performance and development.We’ll discuss why PHP is an awesome programming language, how to make your applications faster in means of performance and development.We’ll discuss why PHP is an awesome programming language, how to make your applications faster in means of performance and development.We’ll discuss why PHP is an awesome programming language, how to make your applications faster in means of performance and development.We’ll discuss why PHP is an awesome programming language, how to make your applications faster in means of performance and development.We’ll discuss why PHP is an awesome programming language, how to make your applications faster in means of performance and development. Why PHP is an awesome language? It’s reliable, — you...",
+    Label: "PHP",
+    ImageUrl: "/assets/contentPic1.jpeg"
+  }
+  
 
 }
