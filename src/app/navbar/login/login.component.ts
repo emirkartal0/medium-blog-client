2f26312cd2762c1e2bfbe5568759ad155ad4d521
emir <emirkartal00@gmail.com>
2023-06-11 23:17:00 +0300

    feat: :ambulance: add session storage service

--- src/app/navbar/login/login.component.ts
+++ src/app/navbar/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { SessionStorageService } from 'src/app/services/session-storage.service';
 
 @Component({
   selector: 'app-login',
@@ -8,8 +10,16 @@ import { NgForm } from '@angular/forms';
 })
 export class LoginComponent {
 
+  constructor(
+    private http: HttpClient,
+    private sessionStorageService: SessionStorageService
+  ) {}
+
+
   login(data: NgForm) {
-    console.log(data);
+    this.http.post<any>('https://localhost:7118/api/Auth/login', data).subscribe(data => {
+      this.sessionStorageService.setItem('token', data.token);
+    })
   }
 
 }
--- src/app/navbar/register/register.component.ts
+++ src/app/navbar/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { SessionStorageService } from 'src/app/services/session-storage.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,16 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  
+  constructor(
+    private http: HttpClient,
+    private sessionStorageService: SessionStorageService
+  ) {}
 
   register(data: NgForm) {
-    console.log(data);
+      this.http.post<any>('https://localhost:7118/api/Auth/register', data).subscribe(data => {
+        this.sessionStorageService.setItem('token', data.token);
+    })
   }
 
 }
