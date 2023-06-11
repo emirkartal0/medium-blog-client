495ffc9c6e9b64f23ed0dec7f6e35f9eb490742b
emir <emirkartal00@gmail.com>
2023-06-11 15:55:42 +0300

    feat: :sparkles: add publish article compenent

--- src/app/edit-article/edit-article.component.ts
+++ src/app/edit-article/edit-article.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-article',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit-article.component.css']
 })
 export class EditArticleComponent {
+
+  newArticle(data: NgForm) {
+    console.log(data);
+  }
 
 }
