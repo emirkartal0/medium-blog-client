2ea528dda915fa1c8617799058b29a8cf206e338
emir <emirkartal00@gmail.com>
2023-06-11 21:19:47 +0300

    feat: :sparkles: change article template and get request

--- src/app/body/articles/articles.component.ts
+++ src/app/body/articles/articles.component.ts
@@ -1,40 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.css']
 })
-export class ArticlesComponent {
+export class ArticlesComponent implements OnInit {
 
-  articles: Array<any> = [
-    {
-      owner: "Emir Kartal",
-      time: "Feb 27",
-      title: "How to make PHP 2000 times faster",
-      description: "We’ll discuss why PHP is an awesome programming language, how to make your applications faster in means of performance and development. Why PHP is an awesome language? It’s reliable, — you...",
-      label: "PHP",
-      minute: "5",
-      picture: "/assets/contentPic1.jpeg"
-    },
-    {
-      owner: "Emir Kartal",
-      time: "Nov 30",
-      title: "7 Unnecessary VSCode Extensions You Should Uninstall Now",
-      description: "The number of VSCode extensions you have installed is one of the main reasons why you might find the editor slow and power-hungry,...",
-      label: "Vscode",
-      minute: "9",
-      picture: "/assets/contentPic2.png"
-    },
-    {
-      owner: "Emir Kartal",
-      time: "May 20",
-      title: "Build and Deploy a Markdown Editor Using Next.js, TypeScript, CodeMirror 6, and react-markdown",
-      description: "Learn to build a Next.js Application. — Following our previous project — Build a Markdown Editor Using Electron, ReactJS, Vite, CodeMirror,...",
-      label: "NextJs",
-      minute: "9",
-      picture: "/assets/contentPic3.png"
-    }
-  ]
+  constructor(
+    private http: HttpClient
+  ) {}
+  articles: Array<any> = [];
+  ngOnInit(): void {
+    this.http.get<any>('https://localhost:7118/api/Article/getArticles').subscribe(data => {
+        console.log(data)
+        this.articles = data;
+    })
+  }
+
+  
 
 }
